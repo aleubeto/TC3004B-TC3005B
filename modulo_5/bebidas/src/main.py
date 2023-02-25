fad7b7a6eca6b8f6f873f65f08e6bf64a3a6949e
ale_wsl <alejo.ubeto@gmail.com>
2023-02-25 10:20:38 -0600

    validate name testing

--- modulo_5/bebidas/src/main.py
+++ modulo_5/bebidas/src/main.py
@@ -1,2 +1,15 @@
+import re
+
+
 def newDrink(drink):
+    return drink.split(',')
+
+
+def validateName(name):
+    alphabetic = len(re.findall("[^a-zA-Z]",name)) == 0
+    length = len(name) >= 2 and len(name) <= 15
+    return alphabetic and length
+
+
+def validateSize():
     return 0
--- modulo_5/bebidas/src/tests/test_main.py
+++ modulo_5/bebidas/src/tests/test_main.py
@@ -1,38 +1,81 @@
 import pytest
-from src.main import newDrink
+from src.main import newDrink, validateName, validateSize
 
-def test_name():
-    # Nombre debe contener solo caracteres alfabéticos
-    # Nombre no puede contener caracteres no alfabéticos
-    # Nombre no puede tener una longitud menor que dos caracteres
-    # Nombre debe tener una longitud de entre 2 a 15 caracteres
-    # Nombre puede una extensión de dos caracteres
-    # Nombre puede tener una extensión de 15 caracteres
-    # Nombre no puede tener una extensión de 1 caracter
-    # Nombre no puede tener una extensión de más de 15 caracteres
-    assert True
+@pytest.mark.parametrize(
+    "input,expected",
+    [
+        # Válido: Nombre debe contener solo caracteres alfabéticos
+        ('abcdefg',True),
+
+        # Válido: Nombre debe tener una longitud de entre 2 a 15 caracteres
+        ('Frescolita',True),
+
+        # Válido: Nombre puede una extensión de dos caracteres
+        ('Lu',True),
+
+        # Válido: Nombre puede tener una extensión de 15 caracteres
+        ('TingleTangoFizz',True),
+
+        # Inválido: Nombre no puede contener caracteres no alfabéticos
+        ('c0cac0!a',False),
+
+        # Inválido: Nombre no puede tener una longitud menor que dos caracteres
+        ('',False),
+
+        # Inválido: Nombre no puede tener una extensión de 1 caracter
+        ('A',False),
+
+        # Inválido: Nombre no puede tener una extensión de más de 15 caracteres
+        ('ChillaCherryCrush',False)
+    ]
+)
+def test_validateName(input,expected):
+    assert validateName(input) == expected
 
 
-def test_size():
-    # Tamaño no puede ser decimal
-    # Tamaño es numérico
-    # Tamaño no puede incluir valores no numéricos
-    # Los tamaños deben estar ordenados de forma ascendente
-    # Los tamaños no pueden no estar ordenados
-    # Deben ingresarse de 1 a 5 tamaños
-    # No pueden haber más de 5 tamaños
-    # Tamaño no puede ser menor que 1
-    # Tamaño debe encontrarse en un rango de 1 a 48
-    # Tamaño no puede ser mayor de 48
-    # Tamaño es un número entero
-    assert True
+@pytest.mark.parametrize(
+    'input,expected',
+    [
+        # Inválido: Tamaño no puede ser decimal
+        ('',True),
 
+        # Válido: Tamaño es numérico
+        ('',True),
 
-def test_string():
+        # Inválido: Tamaño no puede incluir valores no numéricos
+        ('',True),
+
+        # Válido: Los tamaños deben estar ordenados de forma ascendente
+        ('',True),
+
+        # Inválido: Los tamaños no pueden no estár ordenados
+        ('',True),
+
+        # Válido: Deben ingresarse de 1 a 5 tamaños
+        ('',True),
+
+        # Inválido: No pueden haber más de 5 tamaños
+        ('',True),
+
+        # Inválido: Tamaño no puede ser menor que 1
+        ('',True),
+
+        # Válido: Tamaño debe encontrarse en un rango de 1 a 48
+        ('',True),
+
+        # Tamaño no puede ser mayor de 48
+        ('',True),
+
+        # Tamaño es un número entero
+        ('',True)
+    ]
+)
+def test_validateSize(input,expected):
+    assert validateSize(input) == expected
+
     # Nombre debe estár al inicio de la cadena
     # Nombre no puede no estár al inicio de la cadena
     # Se debe separar cada dato con una coma
     # No pueden ingresarse datos si no están separados por coma
     # La entrada puede contener espacios en blanco
     # La entrada puede no tener espacios en blanco
-    assert True
