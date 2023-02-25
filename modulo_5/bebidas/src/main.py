de93d84f8d008f5d2a6cd27e87912fa16c1d24b7
ale_wsl <alejo.ubeto@gmail.com>
2023-02-24 23:23:12 -0600

    test cases added

--- modulo_5/bebidas/src/main.py
+++ modulo_5/bebidas/src/main.py
@@ -1,10 +1,2 @@
-def suma(x,y):
-    return x + y
-
-
-def is_greater_than(n1,n2):
-    return n1 > n2
-
-
-def newDrink():
+def newDrink(drink):
     return 0
--- modulo_5/bebidas/src/tests/test_main.py
+++ modulo_5/bebidas/src/tests/test_main.py
@@ -1,3 +1,38 @@
 import pytest
 from src.main import newDrink
 
+def test_name():
+    # Nombre debe contener solo caracteres alfabéticos
+    # Nombre no puede contener caracteres no alfabéticos
+    # Nombre no puede tener una longitud menor que dos caracteres
+    # Nombre debe tener una longitud de entre 2 a 15 caracteres
+    # Nombre puede una extensión de dos caracteres
+    # Nombre puede tener una extensión de 15 caracteres
+    # Nombre no puede tener una extensión de 1 caracter
+    # Nombre no puede tener una extensión de más de 15 caracteres
+    assert True
+
+
+def test_size():
+    # Tamaño no puede ser decimal
+    # Tamaño es numérico
+    # Tamaño no puede incluir valores no numéricos
+    # Los tamaños deben estar ordenados de forma ascendente
+    # Los tamaños no pueden no estar ordenados
+    # Deben ingresarse de 1 a 5 tamaños
+    # No pueden haber más de 5 tamaños
+    # Tamaño no puede ser menor que 1
+    # Tamaño debe encontrarse en un rango de 1 a 48
+    # Tamaño no puede ser mayor de 48
+    # Tamaño es un número entero
+    assert True
+
+
+def test_string():
+    # Nombre debe estár al inicio de la cadena
+    # Nombre no puede no estár al inicio de la cadena
+    # Se debe separar cada dato con una coma
+    # No pueden ingresarse datos si no están separados por coma
+    # La entrada puede contener espacios en blanco
+    # La entrada puede no tener espacios en blanco
+    assert True
