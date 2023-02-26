3d89091a92a779767ca01e2d57489fb95d4233c0
ale_wsl <alejo.ubeto@gmail.com>
2023-02-26 17:27:26 -0600

    test_validateSize done

--- modulo_5/bebidas/src/main.py
+++ modulo_5/bebidas/src/main.py
@@ -1,15 +1,23 @@
 import re
 
 
-def newDrink(drink):
-    return drink.split(',')
-
-
 def validateName(name):
-    alphabetic = len(re.findall("[^a-zA-Z]",name)) == 0
+    alphabetic = re.search("[^a-zA-Z]",name) is None
     length = len(name) >= 2 and len(name) <= 15
     return alphabetic and length
 
 
-def validateSize():
-    return 0
+def validateSize(sizeArray):
+    numeric = re.search(r"\D",''.join(sizeArray)) is None
+    if not numeric:
+        return False
+    length = len(sizeArray) >= 1 and len(sizeArray) <= 5
+    order = sizeArray == sorted(sizeArray)
+    sizeValues = list(map(lambda x: int(x), sizeArray))
+    sizeRange = min(sizeValues) >= 1 and max(sizeValues) <= 48
+    nonduplicate = len(sizeArray) == len(set(sizeArray))
+    return length and order and sizeRange and nonduplicate
+
+
+def validateString(string):
+    return True
--- modulo_5/bebidas/src/tests/test_main.py
+++ modulo_5/bebidas/src/tests/test_main.py
@@ -1,5 +1,5 @@
 import pytest
-from src.main import newDrink, validateName, validateSize
+from src.main import validateName, validateSize, validateString
 
 @pytest.mark.parametrize(
     "input,expected",
@@ -36,46 +36,69 @@ def test_validateName(input,expected):
 @pytest.mark.parametrize(
     'input,expected',
     [
-        # Inválido: Tamaño no puede ser decimal
-        ('',True),
+        # 0.-Válido: Tamaño es numérico
+        (['2','5','8'],True),
 
-        # Válido: Tamaño es numérico
-        ('',True),
+        # 1.-Válido: Los tamaños deben estar ordenados de forma ascendente
+        (['1','2','3'],True),
 
-        # Inválido: Tamaño no puede incluir valores no numéricos
-        ('',True),
+        # 2.-Válido: Deben ingresarse de 1 a 5 tamaños
+        (['1','2','3','4','5'],True),
 
-        # Válido: Los tamaños deben estar ordenados de forma ascendente
-        ('',True),
+        # 3.-Válido: Tamaño debe encontrarse en un rango de 1 a 48
+        (['1','48'],True),
 
-        # Inválido: Los tamaños no pueden no estár ordenados
-        ('',True),
+        # 4.-Válido: Tamaño es un número entero
+        (['1'],True),
 
-        # Válido: Deben ingresarse de 1 a 5 tamaños
-        ('',True),
+        # 5.-Inválido: Tamaño no puede incluir valores no numéricos
+        (['a','2','3'],False),
 
-        # Inválido: No pueden haber más de 5 tamaños
-        ('',True),
+        # 6.-Inválido: Tamaño no puede tener valores decimales
+        (['1','2.3'],False),
 
-        # Inválido: Tamaño no puede ser menor que 1
-        ('',True),
+        # 7.-Inválido: Los valores de Tamaño no pueden no estár ordenados
+        (['3','2','1'],False),
 
-        # Válido: Tamaño debe encontrarse en un rango de 1 a 48
-        ('',True),
+        # 8.-Inválido: No pueden haber más de 5 tamaños
+        (['1','2','3','4','5','6'],False),
 
-        # Tamaño no puede ser mayor de 48
-        ('',True),
+        # 9.-Inválido: Tamaño no puede tener valores menores que 1
+        (['0'],False),
 
-        # Tamaño es un número entero
-        ('',True)
+        # 10.-Inválido: Tamaño no puede tener valores mayores de 48
+        (['49'],False),
+
+        # 11.-Inválido: Tamaño no puede contener valores repetidos
+        (['1','2','2','3'],False)
+
     ]
 )
 def test_validateSize(input,expected):
     assert validateSize(input) == expected
 
-    # Nombre debe estár al inicio de la cadena
-    # Nombre no puede no estár al inicio de la cadena
-    # Se debe separar cada dato con una coma
-    # No pueden ingresarse datos si no están separados por coma
-    # La entrada puede contener espacios en blanco
-    # La entrada puede no tener espacios en blanco
+
+@pytest.mark.parametrize(
+    'input,expected',
+    [
+        # Válido: Nombre debe estár al inicio de la cadena
+        ('TingleTangoFizz, 2, 5, 8',True),
+
+        # Inválido: Nombre no puede no estár al inicio de la cadena
+        ('2, TingleTangoFizz, 5, 8',False),
+
+        # Válido: Se debe separar cada dato con una coma
+        ('Frescolita, 2, 5, 8',True),
+
+        # Inválido: No pueden ingresarse datos si no están separados por coma
+        ('Frescolita-2-5-8',False),
+
+        # Válido: La entrada puede contener espacios en blanco
+        ('Sprite, 3, 6, 9',True),
+
+        # Válido: La entrada puede no tener espacios en blanco
+        ('Sprite,3,6,9',True)
+    ]
+)
+def test_validateString(input,expected):
+    assert validateString(input) == expected
